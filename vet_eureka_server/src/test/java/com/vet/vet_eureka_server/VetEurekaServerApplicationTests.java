f3b50e248967ce297fca7fe1d0141074ce807666
NickRayan <nickcerron@gmail.com>
2024-08-18 23:19:19 -0500

    vet_eureka_server: init basic config

---
+++ vet_eureka_server/src/test/java/com/vet/vet_eureka_server/VetEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.vet_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
