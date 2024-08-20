656b7eeb74f712947566dc9c8dbb4c7c10324ce2
NickRayan <nickcerron@gmail.com>
2024-08-20 15:06:02 -0500

    vet_perfil_service.config: common utils imported and web clients configured

--- vet_perfil_service/src/main/java/com/vet/perfils/config/WebClientConfig.java
+++ vet_perfil_service/src/main/java/com/vet/perfils/config/WebClientConfig.java
@@ -1,8 +1,10 @@
 package com.vet.perfils.config;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -13,8 +15,16 @@ public class WebClientConfig {
         return WebClient.builder();
     }
 
-    @Bean(name = "clinicWebClient")
+    @Bean
+    @Qualifier("clinicWebClient")
+    @Primary
     WebClient clinicWebClient(WebClient.Builder wBuilder) {
         return wBuilder.baseUrl("http://VET-CLINIC-SERVICE").build();
+    }
+
+    @Bean
+    @Qualifier("userWebClient")
+    WebClient userWebClient(WebClient.Builder wBuilder) {
+        return wBuilder.baseUrl("http://VET-USER-SERVICE").build();
     }
 }
