e5bc8133cbadf0279ae7f093da78e89c6d27a27b
NickRayan <nickcerron@gmail.com>
2024-08-20 12:19:05 -0500

    vet_user_service.AppConfig: property security source added

---
+++ vet_user_service/src/main/java/com/vet/users/config/AppConfig.java
@@ -0,0 +1,8 @@
+package com.vet.users.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:security.properties")
+public class AppConfig {}
