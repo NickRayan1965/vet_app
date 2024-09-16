5165ab462a2d5cbf9336500d3e22e29e5e542be2
NickRayan <nickcerron@gmail.com>
2024-09-16 14:49:21 -0500

    vet_auth_common: properties removed

---
+++ vet_auth_common/src/main/java/com/vet/auth_common/VetAuthCommonApplication.java
@@ -0,0 +1,9 @@
+package com.vet.auth_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetAuthCommonApplication {
+
+}
---
+++ vet_auth_common/src/test/java/com/vet/auth_common/VetAuthCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.auth_common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetAuthCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
